eec7674b73d156ba37cbea1f9673c13a29e07c43
Perseu <josimar.17perseu@gmail.com>
2022-04-03 16:12:11 -0300

    projeto concluido sem nenhum bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 let pedido = Number(0);
 
+let nomeComida;
+let nomeBebida;
+let nomeSobremesa;
+let valorTotal;
+
 function selecionarPrato(element){
    
     const pratoSelecionado = document.querySelector(".comida .selecionado")
@@ -77,5 +82,34 @@ function ativarBotao(){
         btn.disabled = true;
         btn.innerHTML = "selecione os 3 itens para fechar o pedido";
     } 
+}
+
+function enviarPedido(){
+
+    let valor1;
+    let valor2;
+    let valor3;
+    let mensagem= "";
+
+    nomeComida = document.querySelector(".comida .selecionado h5").innerHTML;
+    valor1 = document.querySelector(".comida .selecionado .preco").innerHTML.replace(/R\$ /, "").replace(/,/, ".");
+
+    nomeBebida = document.querySelector(".bebida .selecionado h5").innerHTML;
+    valor2 = document.querySelector(".bebida .selecionado .preco").innerHTML.replace(/R\$ /, "").replace(/,/, ".");;
+
+    nomeSobremesa = document.querySelector(".sobremesa .selecionado h5").innerHTML;
+    valor3 = document.querySelector(".sobremesa .selecionado .preco").innerHTML.replace(/R\$ /, "").replace(/,/, ".");;
+
+    valorTotal = Number(valor1) + Number(valor2) + Number(valor3);
+    
+    mensagem += "Olá, gostaria de fazer o pedido:";
+    mensagem += `\n - Prato: ${nomeComida}`;
+    mensagem += `\n - Bebida: ${nomeBebida}`
+    mensagem += `\n - Sobremesa: ${nomeSobremesa}`
+    mensagem += `\n - Total: R$ ${valorTotal}`
+
+    mensagem = encodeURIComponent(mensagem);
+
+    window.open(`https://wa.me/5573999059366?text=${mensagem}`);
 }
 
